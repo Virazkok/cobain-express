dc6a50347d05d1a3e132af6a51945faa783317c2
Virazkok <vickyhernawan@yahoo.com>
2025-11-20 23:28:31 +0700

    perubahan db.js (2)

--- src/db.js
+++ src/db.js
@@ -90,7 +90,38 @@ const pool = new Pool({
   ssl,
 });
 
+
+let client; // Singleton instance
+
+
+function getClient() {
+if (!client) {
+client = new Client({
+host: process.env.PG_HOST,
+port: process.env.PG_PORT,
+user: process.env.PG_USER,
+password: process.env.PG_PASSWORD,
+database: process.env.PG_DATABASE,
+ssl: {
+rejectUnauthorized: true,
+ca,
+},
+});
+
+
+client.connect().then(() => {
+console.log('Database connected');
+}).catch(err => {
+console.error('DB connection error:', err);
+});
+}
+
+
+return client;
+}
+
 module.exports = {
   query: (text, params) => pool.query(text, params),
   pool,
+  getClient
 };
